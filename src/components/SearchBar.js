19187d3b70172cc651832b8c5616f594be669153
Rex McArthur <drexmcarthur@gmail.com>
2019-12-30 15:32:19 -0700

    Initial Commit

---
+++ src/components/SearchBar.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { Feather, Ionicons } from '@expo/vector-icons'
+
+const SearchBar = ({ term, onTermChange }) => {
+    return (
+        <View style={styles.backgroundStyle}> 
+        <Ionicons style={styles.iconStyle} name="ios-search" />
+        <TextInput
+            autoCapitalize="none"
+            autoCorrect={false}
+            style={styles.inputStyle}
+            placeholder="Search"
+            value={term}
+            onChangeText={newTerm => onTermChange(newTerm)}
+            onEndEditing={() => console.log('submitted')}
+            />
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+    backgroundStyle: {
+        backgroundColor: '#F0EEEE',
+        height: 50,
+        marginHorizontal: 20,
+        marginTop:15,
+        borderRadius:10,
+        flexDirection: 'row'
+    },
+    inputStyle: {
+        borderColor: 'black',
+        borderWidth: 0,
+        flex:1,
+        fontSize: 18
+    },
+    iconStyle: {
+        fontSize: 30,
+        alignSelf: 'center',
+        marginHorizontal: 8,
+    }
+
+
+});
+
+export default SearchBar;
